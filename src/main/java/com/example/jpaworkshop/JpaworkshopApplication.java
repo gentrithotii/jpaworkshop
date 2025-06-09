861f3f361f890090378f873cdbf3e08c9ea7a9c1
Gentrithotii <Gentrithoti@hotmail.com>
2025-06-09 15:03:04 +0200

    Initial commit

---
+++ src/main/java/com/example/jpaworkshop/JpaworkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaworkshop/JpaworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
